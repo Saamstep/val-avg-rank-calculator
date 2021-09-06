0f36cc9721143ff076bd69b16fc5976e6cfb8aa4
Samstep <Samstep2012@gmail.com>
2021-09-06 03:44:46 -0700

    goodnight

--- html/web.js
+++ html/web.js
@@ -64,7 +64,7 @@ function rank() {
 
   let rounded = Math.floor(sum);
 
-  document.getElementById("demo").innerHTML = `<br>${sum}<br>${rounded}<br>${key2[rounded]}`;
+  document.getElementById("demo").innerHTML = `<br><h2>Sum:</h2><p>${sum}</p><h2>Rounded:</h2><p>${rounded}</p><h2>Rank:</h2><p>${key2[rounded]}</p>`;
 }
 
 function newPlayer() {
