ea20fa5b56767a18cd86abcc242e28ebc0ad206c
Samstep <Samstep2012@gmail.com>
2021-09-08 23:09:09 -0700

    last before public

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ app.all("/style.css", (req, res) => {
   res.sendFile(path.join(__dirname + "/html/style.css"));
 });
 
-app.listen(8080, () => console.log("Calculator ready!"));
+app.listen(8888, () => console.log("Calculator ready!\n\nLive at localhost:8888"));
