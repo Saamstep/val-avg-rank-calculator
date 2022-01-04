95b8b75332fdd639acd766ca43b8f032507b8cd8
Samstep <Samstep2012@gmail.com>
2022-01-03 22:41:07 -0800

    spring update

--- app.js
+++ app.js
@@ -15,4 +15,8 @@ app.all("/style.css", (req, res) => {
   res.sendFile(path.join(__dirname + "/html/style.css"));
 });
 
+app.all("/PEL.ico", (req, res) => {
+  res.sendFile(path.join(__dirname + "/PEL.ico"));
+});
+
 app.listen(8888, () => console.log("Calculator ready!\n\nLive at localhost:8888"));
